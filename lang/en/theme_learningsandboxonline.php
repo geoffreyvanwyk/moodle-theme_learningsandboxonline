e116448941c16d04728df817e30f14d5d0ebe102
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2024-01-15 14:48:44 +0200

    feat: new theme based on Boost

---
+++ lang/en/theme_learningsandboxonline.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package    theme_learningsandboxonline
+ * @copyright  2024 Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz> 
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Human-readable name of the plugin.
+$string['pluginname'] = 'Learning Sandbox Online';
+
+// A description of the theme displayed in the theme selector.
+$string['choosereadme'] = 'Theme for online learning sandboxes.';
+
