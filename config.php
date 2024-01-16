bfbbefcbca6ecd4248739cbc11c355fd44edcb2d
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2024-01-16 19:37:19 +0200

    feat: add settings

--- config.php
+++ config.php
@@ -26,13 +26,19 @@ defined('MOODLE_INTERNAL') || die();
 
 $THEME->name = 'learningsandboxonline';
 $THEME->parents = ['boost'];
-$THEME->rendererfactory = 'theme_overridden_renderer_factory'; 
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->sheets = [];
 $THEME->editor_sheets = [];
 $THEME->usefallback = true;
-$THEME->enable_dock = false;    
-$THEME->yuicssmodules = []; 
+$THEME->enable_dock = false;
+$THEME->yuicssmodules = [];
 $THEME->requiredblocks = '';
 $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
 $THEME->haseditswitch = true;
+// This is the function that returns the SCSS source for the main file in our
+// theme. We override the boost version because we want to allow presets
+// uploaded to our own theme file area to be selected in the preset list.
+$THEME->scss = function($theme) {
+    return theme_learningsandboxonline_get_main_scss_content($theme);
+};
 
--- lang/en/theme_learningsandboxonline.php
+++ lang/en/theme_learningsandboxonline.php
@@ -32,3 +32,43 @@ $string['choosereadme'] = 'Theme for online learning sandboxes.';
 
 $string['credsforadmin'] = 'Copy the credentials for the site administrator.';
 
+$string['advancedsettings'] = 'Advanced settings';
+$string['backgroundimage'] = 'Background image';
+$string['backgroundimage_desc'] = 'The image to display as a background of the site. The background image you upload here will override the background image in your theme preset files.';
+$string['brandcolor'] = 'Brand colour';
+$string['brandcolor_desc'] = 'The accent colour.';
+$string['bootswatch'] = 'Bootswatch';
+$string['bootswatch_desc'] = 'A bootswatch is a set of Bootstrap variables and css to style Bootstrap';
+$string['configtitle'] = 'Learning Sandbox Online';
+$string['generalsettings'] = 'General settings';
+$string['loginbackgroundimage'] = 'Login page background image';
+$string['loginbackgroundimage_desc'] = 'The image to display as a background for the login page.';
+$string['nobootswatch'] = 'None';
+$string['presetfiles'] = 'Additional theme preset files';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href="https://docs.moodle.org/dev/Boost_Presets">Boost presets</a> for information on creating and sharing your own preset files, and see the <a href="https://archive.moodle.net/boost">Presets repository</a> for presets that others have shared.';
+$string['preset'] = 'Theme preset';
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+$string['privacy:metadata'] = 'The Learning Sandbox Online theme does not store any personal data about any user.';
+$string['rawscss'] = 'Raw SCSS';
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+$string['rawscsspre'] = 'Raw initial SCSS';
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
+$string['region-side-pre'] = 'Right';
+$string['showfooter'] = 'Show footer';
+$string['unaddableblocks'] = 'Unneeded blocks';
+$string['unaddableblocks_desc'] = 'The blocks specified are not needed when using this theme and will not be listed in the \'Add a block\' menu.';
+$string['privacy:metadata:preference:draweropenblock'] = 'The user\'s preference for hiding or showing the drawer with blocks.';
+$string['privacy:metadata:preference:draweropenindex'] = 'The user\'s preference for hiding or showing the drawer with course index.';
+$string['privacy:metadata:preference:draweropennav'] = 'The user\'s preference for hiding or showing the drawer menu navigation.';
+$string['privacy:drawerindexclosed'] = 'The current preference for the index drawer is closed.';
+$string['privacy:drawerindexopen'] = 'The current preference for the index drawer is open.';
+$string['privacy:drawerblockclosed'] = 'The current preference for the block drawer is closed.';
+$string['privacy:drawerblockopen'] = 'The current preference for the block drawer is open.';
+
+// Deprecated since Moodle 4.0.
+$string['totop'] = 'Go to top';
+
+// Deprecated since Moodle 4.1.
+$string['currentinparentheses'] = '(current)';
+$string['privacy:drawernavclosed'] = 'The current preference for the navigation drawer is closed.';
+$string['privacy:drawernavopen'] = 'The current preference for the navigation drawer is open.';
